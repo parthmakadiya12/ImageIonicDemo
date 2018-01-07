fac93ca697f86db20fbcf84481bb9319e2c4ea9e
Parth Makadiya <parthmakadiya12@gmail.com>
2018-01-07 20:03:33 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Camera } from '@ionic-native/camera';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { File } from '@ionic-native/file';
+import { FilePath } from '@ionic-native/file-path';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
+    FilePath,
+    File,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,138 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Loading, ActionSheetController, LoadingController, Platform, ToastController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { CommentStmt } from '@angular/compiler/src/output/output_ast';
+import { FilePath } from '@ionic-native/file-path';
+import { File } from '@ionic-native/file';
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  lastImage: string = null;
+  loading: Loading;
+  images: Array<{ src: String }>;
+  options: CameraOptions;
+  ngOnInit() {
+    this.options = {
+      quality: 80,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      allowEdit: false,
+      encodingType: this.camera.EncodingType.JPEG,
+      saveToPhotoAlbum: false
+    };
+  }
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController) {
+    this.images = [];
+  }
+  public presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 
+  public takePicture(sourceType) {
+    // Create options for the Camera Dialog
+    console.log("source type "+JSON.stringify(sourceType));
+    var options = {
+      quality: 100,
+      sourceType: sourceType,
+      saveToPhotoAlbum: true,
+      correctOrientation: true
+    };   
+    // Get the data of an image
+    this.camera.getPicture(options).then((imagePath) => {
+      // Special handling for Android library
+      console.log("imagepath 1st entry-"+JSON.stringify(imagePath));  
+      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            console.log("filePath-"+JSON.stringify(filePath));
+            console.log("imagepath-"+JSON.stringify(imagePath));            
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            console.log("correct path "+correctPath);
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
+      } else {
+
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        console.log("else imagepath-"+JSON.stringify(imagePath));
+        console.log("else correct path "+correctPath);
+        console.log("else current name path "+currentName);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      }
+    }, (err) => {
+      this.presentToast('Error while selecting image.');
+    });
+  }
+  private createFileName() {
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+    return newFileName;
+  }
+   
+  // Copy the image to a local folder
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
+  }
+   
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+   //https://devdactic.com/ionic-2-images/ reference by Parth
+  // Always get the accurate path to your apps folder
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + img;
+    }
+  }
+  resize(size) {
+    var x: number = size;
+    // this.options.quality = x;
+    // this.takePhoto();
+    // this.options.targetWidth=x;
+    // this.options.targetHeight=x;    
   }
 
 }
